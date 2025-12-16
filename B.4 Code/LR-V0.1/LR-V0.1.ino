66d89f31cbf12a0db11e77fa25b6d507999e2245
jcchincheong-bb <jc.chincheong@gmail.com>
2025-12-16 01:01:16 +0100

    new sensor logic w/ LoRa - working

---
+++ B.4 Code/LR-V0.1/LR-V0.1.ino
@@ -0,0 +1,199 @@
+/*
+@author: Justin Chin Cheong (34140) and Abhinav Kothari (33349)
+@desc: Complete Script for the Talking Mailbox Project to read all sensors and send data via LoRaWAN to the TTN
+@version: V0.0 - first operating draft
+*/
+
+// ############################### Libraries ######################################
+#include "config.h"
+#include "EEPROM.h"
+#include <Wire.h>
+#include "SensorManager.h"
+
+// ############################### Variables and Configuration ######################################
+// Selecting Europe Region
+const LoRaWANBand_t Region = EU868;
+const uint8_t subBand = 0; 
+
+// SX1262 pin order: Module(NSS/CS, DIO1, RESET, BUSY);
+SX1262 radio = new Module(41, 39, 42, 40);
+
+// create the LoRaWAN node
+LoRaWANNode node(&radio, &Region, subBand);
+
+uint64_t joinEUI =   RADIOLIB_LORAWAN_JOIN_EUI;
+uint64_t devEUI  =   RADIOLIB_LORAWAN_DEV_EUI;
+uint8_t appKey[] = { RADIOLIB_LORAWAN_APP_KEY };
+uint8_t nwkKey[] = { RADIOLIB_LORAWAN_NWK_KEY };
+
+#define LORAWAN_DEV_INFO_SIZE 36
+uint8_t deviceInfo[LORAWAN_DEV_INFO_SIZE] = {0};
+
+#define SERIAL_DATA_BUF_LEN  64
+uint8_t serialDataBuf[SERIAL_DATA_BUF_LEN] = {0};
+uint8_t serialIndex = 0;
+
+#define UPLINK_PAYLOAD_MAX_LEN  256
+uint8_t uplinkPayload[UPLINK_PAYLOAD_MAX_LEN] = {0};
+uint16_t uplinkPayloadLen = 0;
+
+uint32_t previousMillis = 0;
+
+// ############################### Setup ######################################
+void setup() {
+  Serial.begin(115200);
+
+  sensorManager_init();
+
+  // For Storing Device Info
+  if(!EEPROM.begin(LORAWAN_DEV_INFO_SIZE))
+  {
+    Serial.println("Failed to initialize EEPROM");
+    while(1);
+  }
+
+  uint32_t now = millis();
+  while(1)
+  {
+    deviceInfoSet();
+    if(millis() - now >= 5000) break;
+  }
+
+  // Radio Setup
+  deviceInfoLoad();
+  Serial.println(F("\nSetup... "));  
+  Serial.println(F("Initialise the radio"));
+  int16_t state = radio.begin();
+  debug(state!= RADIOLIB_ERR_NONE, F("Initialise radio failed"), state, true);
+
+  // SX1262 rf switch order: setRfSwitchPins(rxEn, txEn);
+  radio.setRfSwitchPins(38, RADIOLIB_NC);
+
+  // Setup the OTAA session information
+  node.beginOTAA(joinEUI, devEUI, nwkKey, appKey);
+  Serial.println(F("Join ('login') the LoRaWAN Network"));
+
+  // Joining TTN 
+  while(1)
+  {
+    state = node.activateOTAA(LORAWAN_UPLINK_DATA_RATE);
+    if(state == RADIOLIB_LORAWAN_NEW_SESSION) break;
+    debug(state!= RADIOLIB_LORAWAN_NEW_SESSION, F("Join failed"), state, true);
+    delay(15000);
+  }
+
+  // Disable the ADR algorithm (on by default which is preferable)
+  node.setADR(false);
+
+  // Set a fixed datarate
+  node.setDatarate(LORAWAN_UPLINK_DATA_RATE);
+
+  // Manages uplink intervals to the TTN Fair Use Policy
+  node.setDutyCycle(false);
+  
+  // Ready to being streaming
+  Serial.println(F("Ready!\n"));
+
+  // Wire.begin();
+}
+// ############################### Main Loop ######################################
+void loop() {
+  // Reading Sensors and Building in Payload
+  uplinkPayloadLen = detectMail(uplinkPayload);
+
+  // Checking if payload is complete
+  if (uplinkPayloadLen >= 2){ // if complete, upload
+    // Printing upload info
+    Serial.print("Uplink payload length: ");
+    Serial.println(uplinkPayloadLen);
+    // Output the uplink payload for debugging
+    Serial.print("Uplink payload: ");
+    for (int i = 0; i < uplinkPayloadLen; i++) {
+      Serial.print(uplinkPayload[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+
+    // Checking LoRa State and Uploading
+    int16_t state = node.sendReceive(uplinkPayload, uplinkPayloadLen, LORAWAN_UPLINK_USER_PORT);
+    if (state!= RADIOLIB_LORAWAN_NO_DOWNLINK && state!= RADIOLIB_ERR_NONE) {
+      Serial.println("Error in sendReceive:");
+      Serial.println(state);
+    } else {
+      Serial.println("Sending uplink successful!");
+    }
+
+    // Checking if it is time to send again
+    uint32_t currentMillis = millis();
+    if(currentMillis - previousMillis >= LORAWAN_UPLINK_PERIOD)
+    {
+      previousMillis = currentMillis;
+      if(uplinkPayloadLen)
+      {
+        Serial.println(F("Sending uplink"));
+        int16_t state = node.sendReceive(uplinkPayload, uplinkPayloadLen, LORAWAN_UPLINK_USER_PORT);
+        debug((state!= RADIOLIB_LORAWAN_NO_DOWNLINK) && (state!= RADIOLIB_ERR_NONE), F("Error in sendReceive"), state, false);
+        uplinkPayloadLen = 0;
+      }
+    }
+  }
+  delay(1000);
+}
+// ############################### Helper Functions ######################################
+void deviceInfoLoad() {
+  uint16_t checkSum = 0, checkSum_ = 0;
+  for(int i = 0; i < LORAWAN_DEV_INFO_SIZE; i++) deviceInfo[i] = EEPROM.read(i);
+  for(int i = 0; i < 32; i++) checkSum += deviceInfo[i];
+  memcpy((uint8_t *)(&checkSum_), deviceInfo + 32, 2);
+
+  if(checkSum == checkSum_)
+  {
+    memcpyr((uint8_t *)(&joinEUI), deviceInfo, 8);
+    memcpyr((uint8_t *)(&devEUI), deviceInfo + 8, 8);
+    memcpy(appKey, deviceInfo + 16, 16);
+
+    Serial.println("Load device info:");
+    Serial.print("JoinEUI:");
+    Serial.println(joinEUI, HEX);
+    Serial.print("DevEUI:");
+    Serial.println(devEUI, HEX);
+    Serial.print("AppKey:");
+    arrayDump(appKey, 16);
+    Serial.print("nwkKey:");
+    arrayDump(nwkKey, 16);
+  }
+  else
+  {
+    Serial.println("Use the default device info as LoRaWAN param");
+  }
+}
+
+void deviceInfoSet() {
+  if(Serial.available())
+  {
+    serialDataBuf[serialIndex++] = Serial.read();
+    if(serialIndex >= SERIAL_DATA_BUF_LEN) serialIndex = 0;
+    if(serialIndex > 2 && serialDataBuf[serialIndex - 2] == '\r' && serialDataBuf[serialIndex-1] == '\n')
+    {
+      Serial.println("Get serial data:");
+      arrayDump(serialDataBuf, serialIndex);
+      if(serialIndex == 34) // 8 + 8 + 16 + 2
+      {
+        uint16_t checkSum = 0;
+        for(int i = 0; i < 32; i++) checkSum += serialDataBuf[i];
+        memcpy(deviceInfo, serialDataBuf, 32);
+        memcpy(deviceInfo + 32, (uint8_t *)(&checkSum), 2);
+        for(int i = 0; i < 34; i++) EEPROM.write(i, deviceInfo[i]);
+        EEPROM.commit();
+        Serial.println("Save serial data, please reboot...");
+      }
+      else
+      {
+        Serial.println("Error serial data length");
+      }
+
+      serialIndex = 0;
+      memset(serialDataBuf, sizeof(serialDataBuf), 0);
+    }
+  }
+}
---
+++ B.4 Code/LR-V0.1/SensorManager.cpp
@@ -0,0 +1,120 @@
+/*
+  Set up and functions for reading and integrating sensors
+*/
+
+// ############################### Libraries ######################################
+#include "SensorManager.h"
+#include <HX711.h>
+
+// ############################### Variables #####################################
+// Variables for Weight Sensor
+HX711 scale;
+uint8_t weightState_curr = 0;
+uint8_t weightState_prev = 0;
+
+// ############################### Functions ######################################
+// Initialising all the sensors
+void sensorManager_init() {
+  // Weight Sensor
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN); // initiliase weight sensor
+  scale.set_scale(940.f); // Set the default (calibrated against scale)
+  scale.tare();
+  scale.power_down(); // Put HX711 to sleep to save power
+  delay(500);
+  SensorReading weightReading = readWeightSensor();
+  weightState_prev = weightReading.value; // initial weight state
+
+  // Light Sensor
+  pinMode(LIGHT_SENSOR_PIN, INPUT); // Input mode for light sensor
+
+  // Tilt Sensor
+  pinMode(TILT_SENSOR_PIN, INPUT); // Input mode for light sensor
+
+  // End Intialisation
+  Serial.println("Sensor Manager Initialized.");
+}
+// To read the Weight Sensor
+SensorReading readWeightSensor() {
+  scale.power_up(); // Wake HX711 from sleep
+  delay(500); // short delay to ensure it wakes in time
+  // Some variables
+  int weightState = 0;
+  long weight = 0;
+  long weight_thres = 15.0; // Threshold [g] at which sensor alerts system
+  
+  SensorReading reading;
+  reading.id = ID_WEIGHT_SENSOR; 
+
+  if (scale.is_ready()) {
+    weight = scale.get_units(10); // Average of 10 readings normalised (based on calibration)
+    if (weight > weight_thres) { // If weight is above our threshold
+      weightState = 1;
+    } else {
+      weightState = 0;
+    }
+  } else {
+    Serial.println("HX711 not found.");
+  }
+
+  reading.value = (uint8_t)weightState; //storing state value
+
+  Serial.print("[Sensor 0x01] Weight: ");
+  Serial.print(weight);
+  Serial.print(" Weight State: ");
+  Serial.println(weightState == HIGH ? "ON (1)" : "OFF (0)");
+
+  scale.power_down(); // Put HX711 to sleep to save power
+  delay(500);
+  return reading;
+}
+// To read the Light Sensor
+SensorReading readLightSensor() {
+  SensorReading reading;
+  reading.id = ID_LIGHT_SENSOR; 
+  
+  int lightState = !digitalRead(LIGHT_SENSOR_PIN);
+  reading.value = (uint8_t)lightState;
+
+  Serial.print("[Sensor 0x02] Light State: ");
+  Serial.println(lightState == HIGH ? "ON (1)" : "OFF (0)");
+  
+  return reading;
+}
+// To read the Tilt Sensor
+SensorReading readTiltSensor() {
+  SensorReading reading;
+  reading.id = ID_TILT_SENSOR; 
+  
+  int tiltState = digitalRead(TILT_SENSOR_PIN);
+  reading.value = (uint8_t)tiltState;
+
+  Serial.print("[Sensor 0x03] Tilt State: ");
+  Serial.println(tiltState == HIGH ? "ON (1)" : "OFF (0)");
+  
+  return reading;
+}
+// To detect new mail and build payload
+uint16_t detectMail(uint8_t* payloadBuffer){
+  uint16_t len = 0;  
+  // Device ID
+  payloadBuffer[len++] = ID_DEVICE;
+  // Read Sensors to detect box opening
+  SensorReading lightReading = readLightSensor();
+  SensorReading tiltReading = readTiltSensor();
+
+  if (lightReading.value && tiltReading.value) { // Box Open
+    delay(5000); // Wait for mail to be placed
+    SensorReading weightReading = readWeightSensor(); // read the weight sensor value
+    weightState_curr = weightReading.value;
+    if (weightState_curr == weightState_prev) { // if no change, then someone is peaking 👀
+      Serial.println("Tampering Detecting");
+      payloadBuffer[len++] = ID_TAMPERING; // Encode Tampering code
+    } else { // If state changed, look at if mail added or removed 
+      Serial.print("Mail was ");
+      Serial.println(weightState_curr == HIGH ? "Added" : "Removed"); // Mail is there?
+      weightState_curr == HIGH ? payloadBuffer[len++] = ID_NEW_MAIL : payloadBuffer[len++] = ID_NO_MAIL;
+    }
+    weightState_prev = weightState_curr; // update weight state
+  } 
+  return len;
+}
